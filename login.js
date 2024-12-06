7cfe4f0acbb0ceb4d3f74ab662629364cf2c30d2
Yonas <142630920+EliasW4444@users.noreply.github.com>
2024-12-05 20:16:34 -0800

    Update login.js

--- login.js
+++ login.js
@@ -3,6 +3,7 @@ const authEndpoint = "https://accounts.spotify.com/authorize";
 
 const clientId = "7fcfb8b43cfa49c395412083cf51bf27";
 // const clientId = "da0bad7f97f941a89d7f8c60e5fcd326";
+const clientId = "a2db107627e644a3b34c1ce7ed8a1ea8";
 
 const scopesArray = [
   "user-modify-playback-state",
